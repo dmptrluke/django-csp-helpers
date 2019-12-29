0262c8e25a6a1865322933dad1705abd7da210eb
Luke Rogers <luke@dmptr.com>
2019-12-29 14:57:43 +1300

    [0.4.2] Version metadata.

---
+++ csp_helpers/__init__.py
@@ -0,0 +1,3 @@
+from .version import __version__  # noqa: F401
+
+default_app_config = "csp_helpers.apps.CSPHelpersConfig"
---
+++ csp_helpers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CSPHelpersConfig(AppConfig):
+    name = 'csp_helpers'
+    verbose_name = 'CSP Helpers'
