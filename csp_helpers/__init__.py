a45c5824b3986f21e91de5a68dd4888f1f1f1102
Luke Rogers <luke@dmptr.com>
2023-08-31 12:13:54 +1200

    [0.7.0] Do new release

--- csp_helpers/__init__.py
+++ csp_helpers/__init__.py
@@ -1,4 +1,4 @@
 """A set of template tags (and mixins!) to assist in building CSP-enabled websites."""
-__version__ = '0.6.2'
+__version__ = '0.7.0'
 
 default_app_config = "csp_helpers.apps.CSPHelpersConfig"
